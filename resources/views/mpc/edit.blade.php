9c2192f6b2ec64d73cf08698eaadd5990801d7df
DarthShelL <shellnosferatu@gmail.com>
2020-02-28 20:03:37 +0300

    slider editor process

--- resources/views/mpc/edit.blade.php
+++ resources/views/mpc/edit.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Edit component</h5>
-            <mpc-edit :id="{{$mpc->id}}"></mpc-edit>
-            </div>
-        </div>
+        <mpc-edit :id="{{$mpc->id}}"></mpc-edit>
     </div>
 @endsection
